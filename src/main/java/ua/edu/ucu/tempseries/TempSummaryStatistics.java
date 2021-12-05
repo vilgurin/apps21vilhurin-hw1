3117bf2aa17785c1f1a52486b2b27d93237cdb48
vilgurin <70943497+vilgurin@users.noreply.github.com>
2021-12-06 00:26:26 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -4,7 +4,21 @@ final public class TempSummaryStatistics {
     private final double devTemp;
     private final double minTemp;
     private final double maxTemp;
-    public TempSummaryStatistics(double avgTemp,double devTemp,double minTemp,double maxTemp){
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+    public double getMinTemp() {
+        return minTemp;
+    }
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp){
         this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
